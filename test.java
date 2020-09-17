4e0a86df4fb75d9bc90a35f885a76d5b46ed95d0
chaitra mv <chaitravenkatesh276@gmail.com>
2020-09-17 12:53:12 +0530

    Added a new line

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println(9 * 91);
+        System.out.println(9 * 11);
+        System.out.println(3 * 4)
     }
 }
 
